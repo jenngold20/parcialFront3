f8a27976fdbcf2cc1942cd23184241b831e6d8d8
jenngold20 <jennifer.goldfeld@usal.edu.ar>
2023-05-31 22:41:09 -0300

    Parcial

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,63 @@
+import React, { useState } from 'react';
+import './App.css';
+import Card from './components/Card';
 
-import './App.css'
-import Card from './components/Card'
+const App = () => {
+  const [input1, setInput1] = useState('');
+  const [input2, setInput2] = useState('');
+  const [showCard, setShowCard] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
 
-function App() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
+    if (input1.trim().length < 3 || input1.trim() === '' || input2.length < 6) {
+      setErrorMessage('Por favor chequea que la información sea correcta');
+      setShowCard(false);
+    } else {
+      setShowCard(true);
+      setErrorMessage('');
+    }
+  };
 
   return (
     <>
-      <div>
-        <h1>Carga de Estudiantes</h1>
-        <form></form>
-        <Card/>
+      <div className="container">
+        <h1>Elegí tu comida favorita</h1>
+        <form className="formulario" onSubmit={handleSubmit}>
+          <label className="input-label">
+            Decime tu nombre:
+            <input
+              type="text"
+              placeholder="Escriba su nombre"
+              className="input-field"
+              value={input1}
+              onChange={(e) => setInput1(e.target.value)}
+            />
+          </label>
+
+          <label className="input-label">
+            ¿Cuál es tu comida favorita?
+            <input
+              type="text"
+              className="input-field"
+              placeholder="Comida favorita"
+              value={input2}
+              onChange={(e) => setInput2(e.target.value)}
+            />
+          </label>
+
+          <button type="submit" className="submit-button">
+            Enviar
+          </button>
+        </form>
+
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
+
+        {showCard && <Card input1={input1} input2={input2} />}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import './Card.css';
 
-const Card = () => {
+const Card = ({ input1, input2 }) => {
   return (
-    <div>Hola , soy una tarjeta</div>
-  )
-}
+    <div className="card">
+      <h2 className="card-title">¡Información ingresada!</h2>
+      <div className="card-content">
+        <p>Tu nombre es: {input1}</p>
+        <p>Tu comida favorita es: {input2}</p>
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
